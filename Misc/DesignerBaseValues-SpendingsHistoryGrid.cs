7b70fd535db46f5c9f2989a0503325f3ff616c49
Unknown <drmcmurderface2@mail.com>
2018-04-07 00:23:26 -0500

    ItemControl Started, going back to DataGrid

---
+++ Misc/DesignerBaseValues-SpendingsHistoryGrid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget_Value
+{
+
+    public class DesignerBaseValues_SpendingsHistoryGrid : Spend
+    {
+
+        #region Quasi-Singleton
+        //public static DesignerBaseValues_SpendingsHistoryGrid Instance { get { return new DesignerBaseValues_SpendingsHistoryGrid(); } }
+        public static DesignerBaseValues_SpendingsHistoryGrid Instance => new DesignerBaseValues_SpendingsHistoryGrid();
+        #endregion
+
+        #region Constructor
+        public DesignerBaseValues_SpendingsHistoryGrid()
+        {
+            this.sTitle = "Cool Title";
+            this.sAdditionalInfo = "Yeah I'm a description";
+            this.vTimestamp = DateTime.Now;
+            this.dAmount = 42.24m;
+        }
+        #endregion
+    }
+}
---
+++ Misc/SpendingsHistoryDataItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Oh no I'm empty
+namespace Budget_Value
+{
+    public class SpendingsHistoryDataItemViewModel
+    {
+    }
+}
